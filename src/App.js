2526040f2c9468da12030a88ca164bc7bde82457
ljubomir-matovic <ljubomir.matovic@quantoxtechnology.com>
2021-11-18 22:03:25 +0100

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Error404 from "pages/Error404";
+import Home from "pages/Home";
+import logo from "images/logo.svg";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <section className="sidebar">
+                <section className="big-square">
+                    <img className="logo" src={logo} alt="logo" />
+                    <section className="small-square"></section>
+                </section>
+            </section>
+            <Router>
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/home" exact element={<Home />} />
+                    <Route path="/invoice/:id" element={<Error404 />} />
+                    <Route element={<Error404 />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Home() {
+    return (
+        <section>
+            <h1>HOME PAGE</h1>
+        </section>
+    );
+}
+export default Home;
