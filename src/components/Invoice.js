d86a6d847d6a4581eba52831eedc42d5e7af26ae
ljubomir-matovic <ljubomir.matovic@quatoxtechnology.com>
2021-11-28 18:49:48 +0100

    single invoice style added

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -91,10 +91,14 @@ function Invoice({ short, id, value }) {
             }} className="mark-as-paid">Mark as paid</button>
             </section>
                 </header>
-
-                <section className="single-card">
-          <section className="single-id-container">
-            <p className="single-id">#{current?.id}</p>
+          <header className="mobile">
+            <section className="status-container absolute-center">
+                Status <Status title={current?.status}/>
+                    </section>
+            </header>
+                <section className="invoice-summary">
+          <section className="invoice-id-container">
+            <h3 className="invoice-id">#{current?.id}</h3>
             <p>{current?.clientName}</p>
           </section>
           <section className="sender-address">
@@ -104,18 +108,18 @@ function Invoice({ short, id, value }) {
             <p>{current?.senderAddress?.country}</p>
           </section>
           <section className="date-due-container">
-            <section className="date-to">
+            <section className="date-due">
               <p>Invoice Date</p>
               <h4>{makeDate(current?.createdAt)}</h4>
             </section>
-            <section className="date-to">
+            <section className="date-due">
               <p>Payment Due</p>
               <h4>{makeDate(current?.paymentDue)}</h4>
             </section>
           </section>
           <section className="client-location">
             <p>Bill To</p>
-            <h2>{current?.clientName}</h2>
+            <h4>{current?.clientName}</h4>
             <p>{current?.clientAddress?.street}</p>
             <p>{current?.clientAddress?.city}</p>
             <p>{current?.clientAddress?.postCode}</p>
@@ -123,11 +127,11 @@ function Invoice({ short, id, value }) {
           </section>
 
           <section className="recepient-mail">
-            <p className="rm-desc">Sent to</p>
-            <p className="rm-email">{current?.clientEmail}</p>
+            <p className="rm-dest">Sent to</p>
+            <h4 className="rm-email">{current?.clientEmail}</h4>
           </section>
-          <section className="items-sum">
-            <section className="item-info item-title">
+          <section className="items-summary">
+            <section className="item-header desktop">
               <section className="item-info-name">
                 <p>{itemsTitles[0]}</p>
               </section>
@@ -141,11 +145,10 @@ function Invoice({ short, id, value }) {
                 <p>{itemsTitles[3]}</p>
               </section>
             </section>
-
-            <section className="items-infos-all">
               {current?.items?.map((item, index) => {
                 return (
-                  <section className="item-info item-properties" key={index}>
+                  <>
+                  <section className="item-info item-properties desktop" key={index}>
                     <section className="item-info-name">
                       <p>{item.name}</p>
                     </section>
@@ -158,16 +161,43 @@ function Invoice({ short, id, value }) {
                     <section className="item-info-total">
                       <p>£{item.total}</p>
                     </section>
-                  </section>
+                    </section>
+                    <section className="item-info item-properties mobile" key={index}>
+                      <section className="absolute-center">
+                    <section className="item-info-name">
+                      <p>{item.name}</p>
+                    </section>
+                    <section className="item-info-quantity">
+                      <p>{item.quantity}x£{item.price}</p>
+                        </section>
+                    </section>
+                    <section className="item-info-total">
+                      <p>£{item.total}</p>
+                    </section>
+                    </section>
+                    </>
                 );
               })}
-            </section>
             <section className="amount-due">
-              <p>Amount Due</p>
-              <p>£{current?.total}</p>
+                <p className="desktop">Amount Due</p>
+                <p className="mobile">Grand Total</p>
+              <h1>£{current?.total}</h1>
             </section>
           </section>
-        </section>
+          </section>
+          <footer className="mobile">
+              <button className="button1 edit-button" onClick={
+                () => { nav(`/invoice/${params.id}/edit-invoice`);}
+            }>Edit</button>
+            <button className="delete" onClick={()=>setDeleteModal(true)}>Delete</button>
+            <button onClick={() => {
+                if(current.status==="pending")
+                {
+                    let a = invoices.map((v, i) => i === Number(params.id) ? { ...v, status: "paid" } : v);
+                    setInvoices(a);
+                }
+            }} className="mark-as-paid">Mark as paid</button>
+          </footer>
       </section>
       </section>
       <Routes>
