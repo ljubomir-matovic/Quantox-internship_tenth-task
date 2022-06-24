e95b4b26fa9eb9beeb71c84531f4cc1a398f529f
ljubomir-matovic <90442628+ljubomir-matovic@users.noreply.github.com>
2022-06-24 13:19:49 +0200

    Update App.js
    console.log

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             let response=await fetch("data.json");
             if(!response.ok)return;
             let responseBody=await response.json();
+            console.log(responseBody);
             setInvoices(responseBody);
         }
     }, []);
